a47d7da24928a16e4cc3a7f8fd01cc4dde5a9ebc
William Fairchild <williamfairchild@Williams-MacBook-Pro.local>
2016-05-25 12:35:12 -0400

    Connected actions and outlets

--- battle/ViewController.swift
+++ battle/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var player1AttackButton: UIButton!
+    @IBOutlet weak var player2AttackButton: UIButton!
+    @IBOutlet weak var character1Image: UIImageView!
+    @IBOutlet weak var character2Image: UIImageView!
+    @IBOutlet weak var feedbackLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +27,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //Actions
+    @IBAction func onPlayer1AttackButtonTapped(sender: UIButton) {
+        
+    }
+    
+    @IBAction func onPlayer2AttackButtonTapped(sender: UIButton) {
+    }
+    
 
 
 }
